fe845b395c7173b9d412d9d46d9f365429ba6088
j55023c <j99509721@gmail.com>
2026-07-30 10:20:17 -0400

    fix: import useMutation from @tanstack/react-query

--- packages/web/src/web/queries/properties.ts
+++ packages/web/src/web/queries/properties.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { supabase } from '../lib/supabase'
 
 export interface PropertyFilters {
